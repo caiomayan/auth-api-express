76399bf52bb90a923c3da6f76dd39a7c5b48b63b
Caio Mayan <caiotensai@gmail.com>
2026-05-07 00:08:37 -0300

    fix: fallback set to resend email, in case of domain is default

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -7,10 +7,10 @@ export const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendOtpEmail(to, code) {
   const { data, error } = await resend.emails.send({
-    from: `Auth API <noreply@${process.env.RESEND_DOMAIN_EMAIL}>`,
+    from: `Auth API <noreply@${process.env.RESEND_DOMAIN_EMAIL || "resend.dev"}>`,
     to: to,
     subject: `Your verification code is: ${code}`,
-    text: `Your verification code is: ${code}`,
+    text: `<h1>Your verification code is: ${code}</h1>`,
   });
 
   if (error) {
